1cf39e8ed707b32e25aaad30c185a4a8e47ae439
Nathan <nathan.hsu@icloud.com>
2018-01-29 16:03:33 -0800

    Initial Commit

---
+++ FizzBuzz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FizzBuzz
+//
+//  Created by Nathan Hsu on 2018-01-29.
+//  Copyright © 2018 Nathan Hsu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
