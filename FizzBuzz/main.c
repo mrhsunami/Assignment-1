0e87c11c267bbc3c568b149a9ea1c24e3d4896f3
Nathan <nathan.hsu@icloud.com>
2018-01-29 16:36:28 -0800

    Assignment 1: FizzBuzz
    Used for loops and if else statements to achieve results

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -9,7 +9,30 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    /* Write a program that prints the numbers from 1 to 100. For the multiples of 3, print "Fizz" instead of the number and for the multiples of 5, print "Buzz" instead of the number. For numbers which are multiples of both 3 and 5 print "FizzBuzz" */
+    
+    /* Pseudocode
+     for 1 to 100 do this:
+        - check if the number is a multiple of 3, if true (if not true, then do next check) then check if it is a multiple of 5. If the second check is also true, then FizzBuzz, break. If not true then Fizz, break
+        - check if number is multi of 5, if true (if not true then do next step), then check if it is a multi of 3. if second check is also true, then FizzBuzz, break. If not true then Buzz, break
+        - print the number, break
+     */
+    
+    for (int i = 1; i < 101; i++) {
+        if (i % 3 == 0 && i % 5 == 0 ) {
+            printf("FizzBuzz\n");
+        }
+        else if (i % 3 == 0) {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0) {
+            printf("Buzz\n");
+        }
+        else {
+            printf("%d\n", i);
+        }
+    }
+    
     return 0;
 }
